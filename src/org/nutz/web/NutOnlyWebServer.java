d1c7957b68f77545ca72a234ba4224c8ddad3a90
Wendal Chen <wendal1985@gmail.com>
2012-12-11 18:51:22 +0800

    fix: Nutz Only Web Server , not URL match

--- src/org/nutz/web/NutOnlyWebServer.java
+++ src/org/nutz/web/NutOnlyWebServer.java
@@ -3,6 +3,9 @@ package org.nutz.web;
 import java.io.IOException;
 
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.session.HashSessionManager;
+import org.eclipse.jetty.server.session.SessionHandler;
+import org.eclipse.jetty.servlet.DefaultServlet;
 import org.eclipse.jetty.servlet.FilterHolder;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.nutz.mvc.NutFilter;
@@ -16,6 +19,10 @@ public class NutOnlyWebServer extends WebServer {
     protected void prepare() throws IOException {
         server = new Server(dc.getAppPort());
         ServletContextHandler ctx = new ServletContextHandler();
+        ctx.setContextPath("/");
+        
+        ctx.addServlet(DefaultServlet.class, "/*");
+        ctx.setSessionHandler(new SessionHandler(new HashSessionManager()));
         
         FilterHolder fh = new FilterHolder(NutFilter.class);
         fh.setInitParameter("modules", dc.get("mainModuleClassName"));
