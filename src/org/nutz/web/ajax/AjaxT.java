9f87a1ce80be40d556a01fa16634bb25e8a80d88
Wendal Chen <wendal1985@gmail.com>
2013-01-08 10:25:48 +0800

    add: 添加一个AjaxT -- 带泛型的AjaxReturn

---
+++ src/org/nutz/web/ajax/AjaxT.java
@@ -0,0 +1,39 @@
+package org.nutz.web.ajax;
+
+public class AjaxT <T> {
+
+	boolean ok;
+	String msg;
+	T data;
+
+	public boolean isOk() {
+		return ok;
+	}
+
+	public String getMsg() {
+		return msg;
+	}
+
+	public T getData() {
+		return data;
+	}
+
+	public AjaxT<T> setOk(boolean ok) {
+		this.ok = ok;
+		return this;
+	}
+
+	public AjaxT<T> setMsg(String msg) {
+		this.msg = msg;
+		return this;
+	}
+
+	public AjaxT<T> setData(T data) {
+		this.data = data;
+		return this;
+	}
+	
+	public static final <T> AjaxT<T> Create(Class<T> klass) {
+		return new AjaxT<T>();
+	}
+}
