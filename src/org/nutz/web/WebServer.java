c7fb786b44227f131c411a7a388e2d0ac856448a
wendal <wendal1985@gmail.com>
2014-12-03 18:43:28 +0800

    fix: android下会绑定ipv6而非ipv4,导致普通应用无法访问

--- src/org/nutz/web/WebServer.java
+++ src/org/nutz/web/WebServer.java
@@ -2,6 +2,8 @@ package org.nutz.web;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.InetSocketAddress;
+
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.webapp.WebAppContext;
 import org.nutz.http.Http;
@@ -37,7 +39,7 @@ public class WebServer {
         if (dc.getAppPort() <= 0) {
             dc.set(WebConfig.APP_PORT, "80");
         }
-        server = new Server(dc.getAppPort());
+        server = new Server(InetSocketAddress.createUnresolved("0.0.0.0", dc.getAppPort()));
         // 设置应用上下文
         String warUrlString = null;
         File root = Files.findFile(dc.getAppRoot());
@@ -58,7 +60,7 @@ public class WebServer {
         server.setHandler(wac);
     }
 
-    void run() {
+    public void run() {
         try {
             prepare();
 
