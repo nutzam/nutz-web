fa728b766e7699382de8841b3cf11cac72b44520
wendal <wendal1985@gmail.com>
2015-01-22 16:32:52 +0800

    ...

--- nutz-web-run.py
+++ nutz-web-run.py
@@ -9,14 +9,20 @@ def main():
         if str(k).startswith("NUTZWEB") :
             print ("%-20s: %s" % (str(k), str(v)))
     
-    print("------------------------------------------------")
-    cp = os.environ.get("NUTZWEB_ETC") + ":" + os.environ.get("key")
+    print "------------------------------------------------"
+    NUTZWEB_ETC = os.environ.get("NUTZWEB_ETC")
+    NUTZWEB_CONF = os.environ.get("NUTZWEB_CONF")
+    if NUTZWEB_ETC and os.path.exists(NUTZWEB_ETC) and os.listdir(NUTZWEB_ETC) :
+        subprocess.call(["cp", "-p" ,NUTZWEB_ETC +"/*", NUTZWEB_CONF+"/"])
+    cp = os.environ.get("NUTZWEB_CONF") + ":" + os.environ.get("NUTZWEB_CLASSES")
     NUTZWEB_LIBS = os.environ.get("NUTZWEB_LIBS")
     for nm in os.listdir(NUTZWEB_LIBS) :
         cp +=  ":" + NUTZWEB_LIBS + "/" + nm
     print "classpath:", cp
     MAIN_CLASS = os.environ.get("NUTZWEB_MAIN_CLASS")
     cmd = "nohup java -cp %s %s %s" % (cp, os.environ.get("NUTZWEB_JAVA_OPTS", ""), MAIN_CLASS)
+    print "CMD: ", cmd
+    print "-------------------------------------------------"
     os.system(cmd)
 
 if __name__ == "__main__" :
